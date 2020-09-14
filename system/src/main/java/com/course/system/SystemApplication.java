53b34448b8db7df89d6bc012845c9430cb71f8ab
“miaogangquyi” <643070111@qq.com>
2020-09-14 22:43:26 +0800

    1.新增system项目为简单springboot项目

---
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -0,0 +1,23 @@
+package com.course.system;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+
+public class SystemApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(SystemApplication.class);
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(SystemApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("启动成功");
+        log.info("System地址\tHttp://127.0.0.1:{}",env.getProperty("server.port"));
+        //SpringApplication.run(EurekaApplication.class, args);
+    }
+
+
+}
