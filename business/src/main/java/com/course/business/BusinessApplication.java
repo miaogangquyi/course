484ebcdb08c6083bebd5daea0499da45e19f1d0b
miaogang <18354255002@163.com>
2020-09-22 16:22:19 +0800

    1.新增generator子模块，用于代码自动生成。 2.Business子模块添加pagehelper，用于自动分页

--- business/src/main/java/com/course/business/BusinessApplication.java
+++ business/src/main/java/com/course/business/BusinessApplication.java
@@ -1,8 +1,8 @@
 package com.course.business;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.SpringApplication;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
--- eureka/src/main/java/com/mg/course/EurekaApplication.java
+++ eureka/src/main/java/com/mg/course/EurekaApplication.java
@@ -1,8 +1,6 @@
 package com.mg.course;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -16,7 +14,7 @@ public class EurekaApplication {
 
     //private static final Logger log = LoggerFactory.getLogger(EurekaApplication.class);
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(EurekaApplication.class);
+        SpringApplication app = new SpringApplication();
         Environment env = app.run(args).getEnvironment();
         log.info("启动成功");
         log.info("Eureka地址\tHttp://127.0.0.1:{}",env.getProperty("server.port"));
