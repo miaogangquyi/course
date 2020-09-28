f62236c67cc30cc5726c6f15ae89db2abbbd4204
miaogang <18354255002@163.com>
2020-09-28 15:30:28 +0800

    1.开启BusinessApplication类的日志

--- business/src/main/java/com/course/business/BusinessApplication.java
+++ business/src/main/java/com/course/business/BusinessApplication.java
@@ -2,6 +2,8 @@ package com.course.business;
 
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -15,12 +17,13 @@ import org.springframework.core.env.Environment;
 @MapperScan("com.course.server.mapper")
 public class BusinessApplication {
 
-    //private static final Logger log = LoggerFactory.getLogger(SystemApplication.class);
+    private static final Logger log = LoggerFactory.getLogger(BusinessApplication.class);
     public static void main(String[] args) {
+
         SpringApplication app = new SpringApplication(BusinessApplication.class);
         Environment env = app.run(args).getEnvironment();
         log.info("启动成功");
-        log.info("Business 地址\tHttp://127.0.0.1:{}",env.getProperty("server.port"));
+        log.info("Business 地址\tHttp://127.0.0.1:{}", env.getProperty("server.port"));
         //SpringApplication.run(EurekaApplication.class, args);
     }
 
