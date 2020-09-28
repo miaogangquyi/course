ce841d76a3b859b2fb573fad10a0cecb8b82ebe7
miaogang <18354255002@163.com>
2020-09-28 21:28:45 +0800

    1.集成sweetalert2，删除时弹出提示框。

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -178,6 +178,36 @@
             },
             del(id) {
                 let _this = this;
+                Swal.fire({
+                    title: '确认删除?',
+                    text: "删除后不可恢复，确认删除？",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/'+id).then((response)=>{
+                            // Loading.hide();
+                            // console.log("save"+JSON.stringify(response))
+                            let resp = response.data;
+                            if (resp.success) {
+                                // $("#form-modal").modal("hide");
+                                _this.list(1);
+                                Swal.fire(
+                                    '删除成功!',
+                                    '删除成功.',
+                                    'success'
+                                )
+                                // Toast.success("保存成功！");
+                                // } else {
+                                //     // Toast.warning(resp.message)
+                            }
+                        })
+
+                    }
+                })
 
                 // 保存校验
                 // if (!Validator.require(_this.chapter.name, "名称")
@@ -187,18 +217,7 @@
                 // _this.chapter.courseId = _this.course.id;
                 //
                 // Loading.show();
-                _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/'+id).then((response)=>{
-                    // Loading.hide();
-                    // console.log("save"+JSON.stringify(response))
-                    let resp = response.data;
-                    if (resp.success) {
-                        // $("#form-modal").modal("hide");
-                        _this.list(1);
-                        // Toast.success("保存成功！");
-                        // } else {
-                        //     // Toast.warning(resp.message)
-                    }
-                })
+
             }
         }
 
