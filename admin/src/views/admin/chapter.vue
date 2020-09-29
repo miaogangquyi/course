c21130d3bff9d3ba6b2a4fe6335aa29b455cdc3c
miaogang <18354255002@163.com>
2020-09-29 15:08:26 +0800

    1.全局校验 是从ControllerAdvice注解和ExceptionHandler补货自定义异常。

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -156,10 +156,10 @@
                 let _this = this;
 
                 // 保存校验
-                if (!Validator.require(_this.chapter.name, "名称")
-                    || !Validator.length(_this.chapter.courseId, "课程ID", 1, 8)) {
-                    return;
-                }
+                // if (!Validator.require(_this.chapter.name, "名称")
+                //     || !Validator.length(_this.chapter.courseId, "课程ID", 1, 8)) {
+                //     return;
+                // }
                 // _this.chapter.courseId = _this.course.id;
                 //
                 // Loading.show();
@@ -171,8 +171,8 @@
                         $("#form-modal").modal("hide");
                         _this.list(1);
                         Toast.success("保存成功！");
-                    // } else {
-                    //     // Toast.warning(resp.message)
+                    } else {
+                        Toast.warning(resp.message)
                     }
                 })
             },
--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,9 +1,10 @@
-package com.course.business.admin.controller;
+package com.course.business.controller.admin;
 
 import com.course.server.dto.ChapterDto;
 import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
 import com.course.server.service.ChapterService;
+import com.course.server.util.ValidatorUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,6 +30,10 @@ public class ChapterController {
 
     @RequestMapping("/save")
     public ResponseDto save(@RequestBody ChapterDto chapterDto) {
+        // 保存校验
+        ValidatorUtil.require(chapterDto.getName(), "名称");
+        ValidatorUtil.require(chapterDto.getCourseId(), "课程ID");
+        ValidatorUtil.length(chapterDto.getCourseId(), "课程ID", 1, 8);
         ResponseDto responseDto = new ResponseDto();
         chapterService.save(chapterDto);
         responseDto.setContent(chapterDto);
