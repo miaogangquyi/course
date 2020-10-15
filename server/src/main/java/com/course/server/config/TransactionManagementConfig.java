bc532316b52bf173a8f765e89d0143a3bdabd986
miaogang <18354255002@163.com>
2020-10-15 17:08:44 +0800

    开启声明示注解

---
+++ server/src/main/java/com/course/server/config/TransactionManagementConfig.java
@@ -0,0 +1,13 @@
+package com.course.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * 开启声明示注解
+ */
+@EnableTransactionManagement
+@Configuration
+public class TransactionManagementConfig {
+
+}
