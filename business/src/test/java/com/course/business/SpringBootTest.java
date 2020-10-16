d0e842e97d9feda0577e5ec781edaecf1d4fbe85
miaogang <18354255002@163.com>
2020-10-16 15:26:17 +0800

    1.分类管理前后端完成 2.引入mybatis-plus-generator

--- business/src/test/java/com/course/business/SpringBootTest.java
+++ business/src/test/java/com/course/business/SpringBootTest.java
@@ -1,5 +1,6 @@
 package com.course.business;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.course.server.domain.Chapter;
 import com.course.server.mapper.ChapterTestMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -23,8 +24,22 @@ public class SpringBootTest {
     public void tes(){
         Chapter chapter = chapterTestMapper.selectById("18");
 
-         //chapterTestMapper.delete(chapter);
-        log.info(chapter.toString());
+        QueryWrapper<Chapter> queryWrapper = new QueryWrapper<>();
+        queryWrapper.getSqlSelect();
+
+
+        //List<Chapter> chapterList = chapterTestMapper.selectBatchIds(Arrays.asList(new String[]{"18", "19"}));
+        //List<Chapter> chapterList = chapterTestMapper.;
+        //List<Chapter> chapterList = chapterTestMapper.selectList(queryWrapper
+                //new QueryWrapper<Chapter>()
+                //.lambda()
+                //.eq(Chapter::getCourseId,"F3LoAsF1")
+                //.eq(Chapter::getName,"大章03")
+        //);
+        //Map<String, Object> map = Maps.newHashMap();
+        //map.put("name", "大章02");
+        //List<Chapter> chapterList = chapterTestMapper.selectByMap(map);
+        //chapterList.forEach(System.out::println);
     }
 }
 
