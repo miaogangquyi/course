7b295ffce51d8dcfcc2fa4ce10e7d37e76326f2e
miaogang <18354255002@163.com>
2020-10-19 15:24:27 +0800

    1.引入ztree 2.新建file模块 3.新建讲师菜单

--- admin/src/router.js
+++ admin/src/router.js
@@ -7,8 +7,8 @@ import Category from "./views/admin/category.vue"
 import Course from "./views/admin/course.vue"
 import Chapter from "./views/admin/chapter.vue"
 import Section from "./views/admin/section.vue"
-// import Content from "./views/admin/content.vue"
-// import Teacher from "./views/admin/teacher.vue"
+import Content from "./views/admin/content.vue"
+import Teacher from "./views/admin/teacher.vue"
 // import File from "./views/admin/file.vue"
 // import User from "./views/admin/user.vue"
 // import Resource from "./views/admin/resource.vue"
@@ -57,7 +57,16 @@ export default new Router({
             path: "business/section",
             name: "business/section",
             component: Section,
-        }]
+        }, {
+            path: "business/teacher",
+            name: "business/teacher",
+            component: Teacher,
+        }, {
+            path: "business/content",
+            name: "business/content",
+            component: Content,
+        }
+        ]
         // , {
     // //          path: "business/section",
     // //         name: "business/section",
